1cb0181e01b4b1144e0422e2dffd07391ec94b24
Leo Merrill <lm755387@southeast.edu>
2023-09-13 14:04:03 -0500

    Fix broken role addition.

--- PCCapstoneFall2023/Program.cs
+++ PCCapstoneFall2023/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>().AddRoles<IdentityRole>();
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
 builder.Services.Configure<IdentityOptions>(options =>
