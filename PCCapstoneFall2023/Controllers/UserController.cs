21dd47d0a51e9635e227fd09469b03dd33a97cad
Leo Merrill <lm755387@southeast.edu>
2023-09-13 14:33:01 -0500

    Create initial user controller

--- PCCapstoneFall2023/Controllers/UserController.cs
+++ PCCapstoneFall2023/Controllers/UserController.cs
@@ -1,12 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using PCCapstoneFall2023.Models;
+using System.Text.RegularExpressions;
 
 namespace PCCapstoneFall2023.Controllers
 {
     public class UserController : Controller
     {
-        public IActionResult Index()
+        private UserManager<User> userManager;
+        private RoleManager<IdentityRole> roleManager;
+
+        public UserController(UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
         {
-            return View();
+            this.userManager = userManager;
+            this.roleManager = roleManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<User> users = new List<User>();
+            foreach (User user in userManager.Users)
+            {
+                user.RoleNames = await userManager.GetRolesAsync(user);
+                users.Add(user);
+            }
+            UserViewModel viewModel = new UserViewModel
+            {
+                Users = users,
+                Roles = roleManager.Roles
+            };
+            return View(viewModel);
         }
     }
 }
