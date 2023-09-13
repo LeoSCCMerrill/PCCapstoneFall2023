87ce295dcf139a0548e566eb03302fa2a721b852
Leo Merrill <lm755387@southeast.edu>
2023-09-13 14:08:01 -0500

    Add user model and user view model

---
+++ PCCapstoneFall2023/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace PCCapstoneFall2023.Models
+{
+    using Microsoft.AspNetCore.Identity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    public class User : IdentityUser
+    {
+        [NotMapped]
+        public IList<string> RoleNames { get; set; }
+    }
+}
---
+++ PCCapstoneFall2023/Models/UserViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace PCCapstoneFall2023.Models
+{
+    public class UserViewModel
+    {
+        public IEnumerable<User> Users { get; set; }
+        public IEnumerable<IdentityRole> Roles { get; set; }
+    }
+}
