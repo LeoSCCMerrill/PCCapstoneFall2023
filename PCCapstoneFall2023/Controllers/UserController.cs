e3079d5aeacae78875bfdceee44309f3474f0e06
Leo Merrill <lm755387@southeast.edu>
2023-09-13 14:18:45 -0500

    Fix to User "using" statements and created UserController

---
+++ PCCapstoneFall2023/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PCCapstoneFall2023.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
