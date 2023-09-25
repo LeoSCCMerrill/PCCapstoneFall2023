ab57b32ec6fbc106e141da81470c3b93c877fc88
Ryan Zimmermann <rz738002@southeast.edu>
2023-09-25 14:48:42 -0500

    Create Drills

--- PCCapstoneFall2023/Controllers/TeacherController.cs
+++ PCCapstoneFall2023/Controllers/TeacherController.cs
@@ -1,17 +1,25 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using PCCapstoneFall2023.Data;
+using PCCapstoneFall2023.Models;
 
 namespace PCCapstoneFall2023.Controllers
 {
-    [Authorize(Roles ="Teacher")]
+    //[Authorize(Roles ="Teacher")]
     public class TeacherController : Controller
     {
+        private readonly ApplicationDbContext _context; 
+        public TeacherController(ApplicationDbContext context) {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
         }
         public IActionResult CreateDrills()
         {
+            Drill dc = new Drill(1, "Test", 2, "yes", DateTime.Now, 2);
+            //List<Drill> drills = _context.Add(dc)
             return View();
         }
         public IActionResult DrillResults()
@@ -22,6 +30,17 @@ namespace PCCapstoneFall2023.Controllers
         public IActionResult TestResults() 
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateDrills([Bind]Drill dc)
+        {
+            
+            if (ModelState.IsValid)
+            {
+                
+            }
+            return View("DrillResults");
         }
     }
 }
